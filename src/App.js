7426a652c2733bd6cb0909cfff3ea10b844b794d
Alfredo Morales <alfredomorales@Alfredos-MacBook-Pro.local>
2023-10-30 09:52:11 -0400

    route changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 
-import Home from "./views/Home";
-import About from "./views/About";
-import Projects from "./views/Projects";
-import Resume from "./views/Resume";
-import Photography from "./views/Photography";
-import SingleProject from "./views/SingleProject";
+import Home from "./views/Home"
+import About from "./views/About"
+import Projects from "./views/Projects"
+import Resume from "./views/Resume"
+import Photography from "./views/Photography"
+import SingleProject from "./views/SingleProject"
 
 
 function App() {
@@ -18,6 +18,10 @@ function App() {
         <Route path="/0/resume" element={<Resume />} />
         <Route path="/0/photography" element={<Photography />} />
         <Route path="/0/projects/:project_name" element={<SingleProject />} />
+        <Route
+          path="*"
+          element={<Home to="/0/home" replace />}
+        />
       </Routes>
     </>
   );

daf8df5a9d74dce5e41b32e6ff025dcb439648c9
Alfredo Morales <33073152+AlfredoMora06@users.noreply.github.com>
2023-11-24 13:02:32 -0500

    adding new font (#24)
    Co-authored-by: Alfredo Morales <alfredomorales@Alfredos-MacBook-Pro.local>

--- src/components/PhotoGrid.tsx
+++ src/components/PhotoGrid.tsx
@@ -1,10 +1,9 @@
 import Grid from "@mui/material/Grid"
+import { Fade, ImageList, ImageListItem, useMediaQuery, useTheme } from "@mui/material"
 
 //@ts-ignore
 import Container from "./Container"
 import { Photo } from "../types/Photo"
-import { Fade, ImageList, ImageListItem } from "@mui/material"
-
 
 type PhotoGridProps = {
   photos: Photo[]
@@ -13,12 +12,32 @@ type PhotoGridProps = {
 export default function PhotoGrid(
   {photos}: PhotoGridProps 
 ):JSX.Element {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"))
 
   return (
     <Container>
       <Grid container>
         { photos.length > 0
-          ? <ImageList variant="masonry" cols={3} gap={8}>
+          ? isMobile
+          ? photos.map((photo) => {
+            const {src} = photo
+            return (
+              <Fade in={true} timeout={800} key={photo.src}>
+                <Grid item xs={12}>
+                  <img
+                    src={`${src}?w=161&fit=crop&auto=format`}
+                    alt={"missing"}
+                    loading="lazy"
+                    placeholder="blur"
+                    width={"100%"}
+                    style={{paddingBottom: 10}}
+                  />
+                </Grid>
+              </Fade>
+            )
+          })
+          : <ImageList variant="masonry" cols={3} gap={8}>
             {photos.map((photo) => {
               const {src} = photo
               return (
--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import { I18nextProvider } from "react-i18next"
 import { persistStore } from "redux-persist"
 import { PersistGate } from "redux-persist/integration/react"
 import { SnackbarProvider } from "notistack"
+import { ThemeProvider, StyledEngineProvider } from "@mui/material"
 
 import "./index.css"
 import App from "./App"
 import store from "./store"
 import {initializeI18next} from "./translations"
+import theme from "./theme"
 
 let persistor = persistStore(store)
 
@@ -19,17 +21,21 @@ initializeI18next()
 
 ReactDOM.render(
   <React.StrictMode>
-    <SnackbarProvider>
-      <I18nextProvider i18n={i18next}>
-        <ReduxProvider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </PersistGate>
-        </ReduxProvider>
-      </I18nextProvider>
-    </SnackbarProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <SnackbarProvider>
+          <I18nextProvider i18n={i18next}>
+            <ReduxProvider store={store}>
+              <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                  <App />
+                </BrowserRouter>
+              </PersistGate>
+            </ReduxProvider>
+          </I18nextProvider>
+        </SnackbarProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
